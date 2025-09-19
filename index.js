9bff12b9e4cd62c30f5d4dca1a04e91a256d4136
gcrisafu <25827246+gcrisafu@users.noreply.github.com>
2025-09-19 12:12:20 -0400

    created event listener to submit a number then used that number to calculate the amount of divs, and how big they should be compared to the height and width

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
-function grid(num){
-    
-    const container = document.getElementById("container");
-    container.style.display = "flex";
-    container.style.flexWrap = "wrap"
-    container.style.width = "800px";
-    container.style.height = "800px";
-   
-    for (let i = 0; i < num * num; i++){ 
-    const div = document.createElement("div");
-        div.classList.add("divs");
-        container.appendChild(div);
-        div.style.width = "50px";
-        div.style.height = "50px";
+const container = document.getElementById("container");
+const divContainer = document.createElement("div");
 
+container.appendChild(divContainer);
+container.style.display = "flex";
+
+divContainer.style.display = "flex";
+divContainer.style.flexWrap = "wrap"
+divContainer.style.width = "960px";
+divContainer.style.height = "960px";
+divContainer.style.flex = "1"
+
+function grid(num){
+
+    // clears the screen
+    divContainer.innerHTML = "";
+    // Creating all the divs   
+    for (let i = 0; i < num * num; i++){ 
+    const div = document.createElement("div"); 
+        div.classList.add("divs");
+        divContainer.appendChild(div);
+    div.style.width = `${100 / num}%`;
+    div.style.height = `${100 / num}%`;
         }
 
-
-        const divAll = document.querySelectorAll("div")
-        divAll.forEach(div => {
-        div.addEventListener('mouseenter', () => {
-        div.style.backgroundColor = "black";
-        })
+    // Mouse over change to black
+    const divAll = document.querySelectorAll(".divs")
+    divAll.forEach(div => {
+    div.addEventListener('mouseenter', () => {
+    div.style.backgroundColor = "black";
+    })
         
     })
     }
-        
+
+
+    const btn = document.createElement("button"); 
+    const userInput = document.createElement('input');
+    userInput.type = 'number';
+    btn.textContent = "Submit";
+    btn.addEventListener("click", () => {
+        const value = userInput.value
+        if(value <= 0){
+            alert("Number needs to be greater than 0")
+        } else if (value > 100) {
+            alert("Number needs to be lower than 100")
+        } else {
+        grid(value)}})
+        ;
+        userInput.placeholder = "# of Rows";
+  
+    container.appendChild(btn);
+    container.appendChild(userInput);
+
     
 
-grid(16);
+
+
 
 
 
